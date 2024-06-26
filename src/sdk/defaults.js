cf04bf8abab20647e60a77c2c82a2be18f9c3eb1
moyuderen <moyuderen>
2024-06-26 15:05:26 +0800

    refactor: 整理代码

--- src/sdk/defaults.js
+++ src/sdk/defaults.js
@@ -2,18 +2,15 @@ const options = {
   target: 'https://jsonplaceholder.typicode.com/posts',
   multipart: true, // 是否分片上传，false时单文件上传
   withCredentials: true,
-  headers: {
-    name: 'hah'
-  },
+  headers: {},
   data: {},
-  concurrency: 10,
+  concurrency: 6,
   chunkSize: 2 * 1024 * 1,
   autoUpload: true,
   name: 'file',
   generateUniqueIdentifier: null,
   successStatuses(xhr) {
     return [200, 201, 202].includes(xhr.status)
-    // return xhr.status === 200
   },
   retries: 3,
   retryInterval: 1000,
