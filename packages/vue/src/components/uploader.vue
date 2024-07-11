cc0ed5815ebadf182364f55b5a79a90452bb4f3d
moyuderen <moyuderen>
2024-07-11 18:35:33 +0800

    feat: vue

---
+++ packages/vue/src/components/uploader.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="uploader">
+    <uploader-drop :multiple="multiple" :accept="accept">
+      <span style="margin-right: 6px">Drop file here or</span>
+      <uploader-btn> click to upload </uploader-btn>
+    </uploader-drop>
+    <slot v-bind:fileList="files">
+      <uploader-list :file-list="files">
+        <template v-slot="slotProps">
+          <uploader-file :file="slotProps.file"></uploader-file>
+        </template>
+      </uploader-list>
+    </slot>
+  </div>
+</template>
+
+<script>
+import Uploader from '@tinyuploader/sdk'
+import UploaderDrop from './uploader-drop.vue'
+import UploaderBtn from './uploader-btn.vue'
+import UploaderList from './uploader-list.vue'
+import UploaderFile from './uploader-file.vue'
+
+const Events = Uploader.Events
+
+export default {
+  components: {
+    UploaderDrop,
+    UploaderBtn,
+    UploaderList,
+    UploaderFile
+  },
+  provide() {
+    return {
+      instance: () => this.uploader
+    }
+  },
+  props: {
+    multiple: {
+      type: Boolean,
+      default: true
+    },
+    accept: {
+      type: String,
+      default: '*'
+    },
+    multiple: {
+      type: Boolean,
+      default: true
+    },
+    accept: {
+      type: String,
+      default: '*'
+    },
+    target: {
+      type: String
+    },
+    withCredentials: {
+      type: Boolean,
+      default: true
+    },
+    headers: Object,
+    data: Object,
+    concurrency: {
+      type: Number,
+      default: 6
+    },
+    chunkSize: {
+      type: Number,
+      default: 1024 * 4
+    },
+    autoUpload: {
+      type: Boolean,
+      default: true
+    },
+    name: {
+      type: String,
+      default: 'file'
+    },
+    generateUniqueIdentifier: {
+      type: [Function, null],
+      default: null
+    },
+    successStatuses: {
+      type: Function
+    },
+    retries: {
+      type: Number,
+      default: 3
+    },
+    retryInterval: {
+      type: Number,
+      default: 1000
+    },
+    merge: {
+      type: Function
+    }
+  },
+  data() {
+    return {
+      uploader: null,
+      files: []
+    }
+  },
+  created() {
+    this.uploader = new Uploader()
+
+    this.uploader.on(Events.FilesAdded, (fileList) => {
+      this.files = fileList
+      this.$emit('onFilesAdded', fileList)
+    })
+
+    this.uploader.on(Events.AllFileSuccess, (fileList) => {
+      this.files = fileList
+      this.$emit('onAllFileSuccess', fileList)
+    })
+
+    this.uploader.on(Events.FileUploadSuccess, (file, fileList) => {
+      this.files = fileList
+      this.$emit('onFileUploadSuccess', file, fileList)
+    })
+
+    this.uploader.on(Events.FileSuccess, (file, fileList) => {
+      this.files = fileList
+      this.$emit('onFileSuccess', file, fileList)
+    })
+
+    this.uploader.on(Events.FileFail, (file, fileList) => {
+      this.files = fileList
+      this.$emit('onFileFail', file, fileList)
+    })
+
+    this.uploader.on(Events.FileMergeFail, (file, fileList) => {
+      this.files = fileList
+      this.$emit('onFileMergeFail', file, fileList)
+    })
+
+    this.uploader.on(Events.FileRemove, (file, fileList) => {
+      this.files = fileList
+      this.$emit('onFileRemove', file, fileList)
+    })
+
+    this.uploader.on(Events.FileProgress, (progress, file, fileList) => {
+      this.files = fileList
+      this.$emit('onFileProgress', file, fileList)
+    })
+  },
+  methods: {
+    abort(id) {
+      if (id) {
+        this.uploader.remove(id)
+        return
+      }
+      this.uploader.remove()
+    },
+    submit() {
+      this.uploader.submit()
+    }
+  }
+}
+</script>
---
+++ packages/vue/vite.config.js
@@ -0,0 +1,29 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue2 from '@vitejs/plugin-vue2'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue2()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      entry: './src/index.js',
+      name: 'TinyUploaderVue',
+      fileName: 'tinyuploader-vue'
+    },
+    minify: true,
+    cssCodeSplit: false,
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: 'Vue'
+      }
+    }
+  }
+})
