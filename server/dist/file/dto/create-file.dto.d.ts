4d5a93e96b7df4104816c95b514e5e0822258234
moyuderen <moyuderen>
2025-05-15 12:25:20 +0800

    ci: vercel

--- server/dist/file/dto/create-file.dto.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class UploadFileDto {
-    file: File;
-    filename: string;
-    index: number;
-    hash: string;
-}
--- server/dist/file/dto/create-file.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UploadFileDto = void 0;
-class UploadFileDto {
-}
-exports.UploadFileDto = UploadFileDto;
-//# sourceMappingURL=create-file.dto.js.map
